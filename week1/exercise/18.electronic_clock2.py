8e23061fa5eb73ba65126284527c63ecf9d5f76c
skv-sergey <68821021+skv-sergey@users.noreply.github.com>
2020-08-09 22:10:44 +0300

    1 test

--- week1/exercise/18.electronic_clock2.py
+++ week1/exercise/18.electronic_clock2.py
@@ -1,5 +1,7 @@
 n = int(input())
 hour = (n // 3600) % 24
-minute = (n // 60) % 60
-second = n % 60
-print(hour, ":", minute, ":", second, sep="")
+mMajor = ((n // 60) % 60) // 10
+mMinor = ((n // 60) % 60) % 10
+sMajor = (n % 60) // 10
+sMinor = (n % 60) % 10
+print(hour, ":", mMajor, mMinor, ":", sMajor, sMinor, sep="")
